afd153ec65da3a0cc0e44d6659c4abf25ab9831e
Joe95-ux <59031763+Joe95-ux@users.noreply.github.com>
2022-02-20 02:30:57 +0100

    fix port issue in server

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ io.on("connection", (socket)=>{
 
 })
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, "0.0.0.0", ()=>{
     console.log(`server is running on port ${PORT}`);
